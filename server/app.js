f6627a29e826b44a7ec245b92f46629d17dd2f4a
Ryan Pencak <rvp002@bucknell.edu>
2018-03-31 22:48:54 -0400

    deploy

--- server/app.js
+++ server/app.js
@@ -1,15 +1,20 @@
 const sqlite3 = require('sqlite3').verbose();
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const path = require('path');
 const dbPath = __dirname + '/db/movieRatings.db';
 // const db = new sqlite3.Database('dbPath');
+
 var db = new sqlite3.Database(dbPath, (err) => {
   if (err) {
     return console.error(err.message);
   }
   console.log('Connected to the in-memory SQlite database.');
 });
+
+app.disable('x-powered-by');
+app.use(bodyParser.json());
 
 app.get('/api/users', function(req, res){
   let sql = `SELECT DISTINCT user_id as UserID, user_name as Name, state as State FROM users`;
@@ -49,6 +54,37 @@ app.get('/api/movies', function(req, res){
 //     });
 // });
 
+app.use(express.static(path.resolve(__dirname, '..', 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+});
+
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, _req, res, _next) => {
+  console.log(err);
+  if (err.status) {
+    return res
+    .status(err.status)
+    .send(err.errors[0].messages[0]);
+  }
+
+  if (err.output && err.output.statusCode) {
+    return res
+    .status(err.output.statusCode)
+    .set('Content-Type', 'text/plain')
+    .send(err.message);
+  }
+
+  console.error(err.stack);
+  res.sendStatus(500);
+});
 
 app.listen(3001);
 
